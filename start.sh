3c62e4f288e11d379484330e094cc2d792d5fdf6
Manthan <mnthn.ai@gmail.com>
2019-11-22 23:47:12 +0530

    glitch bug fix

--- start.sh
+++ start.sh
@@ -13,6 +13,8 @@ if [ ! -f $VIRTUALENV/bin/pip ]; then
   curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
 fi
 
-$VIRTUALENV/bin/pip install -r requirements.txt
+$VIRTUALENV/bin/pip install flask
+$VIRTUALENV/bin/pip install numpy
+$VIRTUALENV/bin/pip install opencv
 
 $VIRTUALENV/bin/python3 src/app.py
